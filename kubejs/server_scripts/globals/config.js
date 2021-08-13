a92b0dae77c9b19fdbf6b7a1dcaca24448d15ebf
kotakotik22 <61428759+kotakotik22@users.noreply.github.com>
2021-08-13 06:45:41 +0300

    gear rotation config

---
+++ kubejs/server_scripts/globals/config.js
@@ -0,0 +1,11 @@
+global.config = {
+    gearRotation: {
+        blocks: [
+            "creategears:gear"
+        ],
+        generatedStress: 8,
+        generatedSpeed: 32,
+        timeActivated: 60,
+        quest: '2A7D482145E6BE2F'
+    }
+}
--- kubejs/server_scripts/mechanics/gear_rotation.js
+++ kubejs/server_scripts/mechanics/gear_rotation.js
@@ -1,10 +1,4 @@
-const gearId = 'creategears:gear'
-const tagId = "kubejs:rotatable"
-const generatedStress = 8
-const generatedSpeed = 32
-const timeActivated = 60;
-const quest = '2A7D482145E6BE2F'
-
+const config = global.config.gearRotation
 var activated = {}
 
 function setTileData(block, tile, data) {
@@ -40,7 +34,7 @@ function setSpeed(block, speed) {
         // translaton: tile.setNetwork(tile.getBlockPos().asLong())
         tile.setNetwork(tile.func_174877_v().func_218275_a())
         let network = java("com.simibubi.create.Create").TORQUE_PROPAGATOR.getOrCreateNetworkFor(tile)
-        const capacity = generatedStress * generatedSpeed
+        const capacity = config.generatedStress * config.generatedSpeed
         network.updateCapacityFor(tile, capacity) // idk...
         data = block.getEntityData(); // update data after its changed by the network
         // oh its so late i start writing comments instead of code again
@@ -56,20 +50,22 @@ function setSpeed(block, speed) {
     return data.Speed != 0
 }
 
-onEvent('block.tags', event => {
-    event.add(tagId, gearId)
-  })
-
 onEvent("block.break", event => {
     let toDelete = posToObj(event.getBlock().pos, event.getEntity().getWorld().getDimension())
     removeActivated(event.getEntity().getWorld(), toDelete, JSON.stringify(toDelete))
 })
 
 onEvent("block.right_click", event => {
-    if(event.block.hasTag(tagId)) {
-        if(setSpeed(event.block, generatedSpeed)) {
-            event.getPlayer().data.ftbquests.complete(quest)
-            activated[posToObj(event.block.pos, event.block.getDimension())] = timeActivated;
+    var blockId = event.block.getId()
+    for(var i in config.blocks) {
+        var id = config.blocks[i]
+        if(id == blockId) {
+            isRotatable = true        
+            if(setSpeed(event.block, config.generatedSpeed)) {
+                event.getPlayer().data.ftbquests.complete(config.quest)
+                activated[posToObj(event.block.pos, event.block.getDimension())] = config.timeActivated;
+            }
+            break
         }
     }
 })
@@ -81,7 +77,7 @@ onEvent("world.tick", event => { // TODO: optimize by using server.tick and gett
         if(event.getWorld().getDimension() == pos.world) {
             activated[posJson]--;
             var ticks = activated[posJson]
-            setSpeed(event.getWorld().getBlock(pos.x, pos.y, pos.z), generatedSpeed)
+            setSpeed(event.getWorld().getBlock(pos.x, pos.y, pos.z), config.generatedSpeed)
             if(ticks <= 0) {
                 toDelete.push(posJson)
             }
